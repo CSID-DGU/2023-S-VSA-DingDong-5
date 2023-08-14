2f719342af2470b0530b14d3febdfe6de4b7687f
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-08-15 02:39:21 +0900

    Refactor: User GET, UPDATE API 로직 분리

---
+++ server/controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+// Response Handler
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true },
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json('You can update only your account!');
+  }
+};
+
+module.exports = {
+  getUser,
+  updateUser,
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,42 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Question = require('../models/Question');
-const bcrypt = require('bcrypt');
+const userController = require('../controllers/userController');
 
 // GET
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get('/:id', userController.getUser);
 
 // UPDATE
-router.put('/:id', async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
-    }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true },
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
-    res.status(401).json('You can update only your account!');
-  }
-});
+router.put('/:id', userController.updateUser);
 
 // DELETE
 router.delete('/:id', async (req, res) => {
